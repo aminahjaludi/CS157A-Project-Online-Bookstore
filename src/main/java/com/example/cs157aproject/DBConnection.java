8e1dab2e9a06303ce55b2f030de00a6e93afe8e7
Charlene Khun <charlenek2009@gmail.com>
2024-12-08 01:28:55 -0800

    Created a completely new database

--- src/main/java/com/example/cs157aproject/DBConnection.java
+++ src/main/java/com/example/cs157aproject/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/test_schema";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/new_schema";
     private static final String USER = "root"; // Replace with your MySQL username
     private static final String PASSWORD = "milk2000A"; // Replace with your MySQL password
 
